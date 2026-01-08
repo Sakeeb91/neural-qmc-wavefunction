3dc112b5786d5b863226f74edf846eadd2cfe8bb
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-08 01:22:01 -0500

    Add convenience imports to main nqmc package
    Enable simple import pattern:
    from nqmc import SimpleWavefunction, VMCOptimizer, hydrogen_molecule
    
    Exports all main classes and functions for easy access.

--- src/nqmc/__init__.py
+++ src/nqmc/__init__.py
@@ -2,6 +2,38 @@
 
 A research-grade implementation of neural network wavefunctions for
 Variational Monte Carlo, targeting hydrogen chain molecules.
+
+Quick Start:
+    >>> from nqmc import SimpleWavefunction, VMCOptimizer, hydrogen_molecule
+    >>> molecule = hydrogen_molecule()
+    >>> wf = SimpleWavefunction()
+    >>> optimizer = VMCOptimizer(learning_rate=1e-3)
 """
 
 __version__ = "0.1.0"
+
+# Convenience imports for common usage
+from nqmc.hamiltonian import local_energy
+from nqmc.optimizer import VMCOptimizer
+from nqmc.sampler import MetropolisSampler
+from nqmc.systems import Molecule, hydrogen_atom, hydrogen_chain, hydrogen_molecule
+from nqmc.wavefunction import SimpleWavefunction, Wavefunction
+
+__all__ = [
+    # Version
+    "__version__",
+    # Systems
+    "Molecule",
+    "hydrogen_molecule",
+    "hydrogen_chain",
+    "hydrogen_atom",
+    # Wavefunction
+    "Wavefunction",
+    "SimpleWavefunction",
+    # Hamiltonian
+    "local_energy",
+    # Sampler
+    "MetropolisSampler",
+    # Optimizer
+    "VMCOptimizer",
+]
