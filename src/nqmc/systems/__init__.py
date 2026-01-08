eb830868acd62375afc2491120cbdbb3ba622ddc
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-08 01:21:43 -0500

    Export molecular system classes from package
    Add public API exports:
    - Molecule, hydrogen_molecule, hydrogen_chain, hydrogen_atom
    - ELEMENT_CHARGES, BOHR_TO_ANGSTROM constants

--- src/nqmc/systems/__init__.py
+++ src/nqmc/systems/__init__.py
@@ -1,1 +1,26 @@
-"""Molecular system definitions."""
+"""Molecular system definitions.
+
+This module provides molecular system dataclasses for quantum Monte Carlo.
+
+Example:
+    >>> from nqmc.systems import hydrogen_molecule, hydrogen_chain
+    >>> h2 = hydrogen_molecule(bond_length=1.4)
+    >>> h4 = hydrogen_chain(n_atoms=4)
+"""
+from nqmc.systems.molecule import (
+    BOHR_TO_ANGSTROM,
+    ELEMENT_CHARGES,
+    Molecule,
+    hydrogen_atom,
+    hydrogen_chain,
+    hydrogen_molecule,
+)
+
+__all__ = [
+    "Molecule",
+    "hydrogen_molecule",
+    "hydrogen_chain",
+    "hydrogen_atom",
+    "ELEMENT_CHARGES",
+    "BOHR_TO_ANGSTROM",
+]
