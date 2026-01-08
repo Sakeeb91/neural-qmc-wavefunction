e8f439e23c07109b27fe940e06f4e42352eebcb3
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-08 01:17:13 -0500

    Export Hamiltonian functions from package
    Add public API exports:
    - local_energy, local_energy_batched
    - kinetic_energy
    - electron_nuclear_potential, electron_electron_potential

--- src/nqmc/hamiltonian/__init__.py
+++ src/nqmc/hamiltonian/__init__.py
@@ -1,1 +1,26 @@
-"""Molecular Hamiltonian operators."""
+"""Molecular Hamiltonian operators.
+
+This module provides Hamiltonian operators for computing local energy
+in variational Monte Carlo calculations.
+
+Example:
+    >>> from nqmc.hamiltonian import local_energy
+    >>> e_local = local_energy(wf, params, r, molecule)
+"""
+from nqmc.hamiltonian.molecular import (
+    EPSILON,
+    electron_electron_potential,
+    electron_nuclear_potential,
+    kinetic_energy,
+    local_energy,
+    local_energy_batched,
+)
+
+__all__ = [
+    "local_energy",
+    "local_energy_batched",
+    "kinetic_energy",
+    "electron_nuclear_potential",
+    "electron_electron_potential",
+    "EPSILON",
+]
