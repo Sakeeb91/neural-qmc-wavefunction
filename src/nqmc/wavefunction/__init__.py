b83c58867aab055fa62fad58f3b6ed682bc530f5
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-08 01:15:23 -0500

    Export wavefunction classes from package
    Add public API exports for wavefunction module:
    - Wavefunction (abstract base class)
    - Params (type alias)
    - SimpleWavefunction and SimpleMLP

--- src/nqmc/wavefunction/__init__.py
+++ src/nqmc/wavefunction/__init__.py
@@ -1,1 +1,20 @@
-"""Neural network wavefunction architectures."""
+"""Neural network wavefunction architectures.
+
+This module provides wavefunction ansätze for variational Monte Carlo.
+
+Available wavefunctions:
+- SimpleWavefunction: Basic MLP for testing (no antisymmetry)
+
+Example:
+    >>> from nqmc.wavefunction import SimpleWavefunction
+    >>> wf = SimpleWavefunction(hidden_dims=(32, 32))
+"""
+from nqmc.wavefunction.base import Params, Wavefunction
+from nqmc.wavefunction.simple import SimpleMLP, SimpleWavefunction
+
+__all__ = [
+    "Wavefunction",
+    "Params",
+    "SimpleWavefunction",
+    "SimpleMLP",
+]
