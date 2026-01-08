a317ebeb234ee9a208e313d0e84e193026cfc781
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-08 01:18:44 -0500

    Export sampler classes from package
    Add public API exports:
    - MetropolisSampler
    - SamplerState

--- src/nqmc/sampler/__init__.py
+++ src/nqmc/sampler/__init__.py
@@ -1,1 +1,15 @@
-"""MCMC samplers for VMC."""
+"""MCMC samplers for VMC.
+
+This module provides Markov Chain Monte Carlo samplers for generating
+electron configurations distributed according to |ψ|².
+
+Example:
+    >>> from nqmc.sampler import MetropolisSampler
+    >>> sampler = MetropolisSampler(step_size=0.1)
+"""
+from nqmc.sampler.metropolis import MetropolisSampler, SamplerState
+
+__all__ = [
+    "MetropolisSampler",
+    "SamplerState",
+]
